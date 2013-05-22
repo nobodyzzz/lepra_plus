1a819c2109987dadbc7f9976b6b873aed72fd5dc
Stas Turlo <stanislav.turlo@rightscale.com>
2013-05-22 23:00:53 +0300

    v0.1.4

--- main.js
+++ main.js
@@ -1,48 +1,50 @@
-String.prototype.fmt = function () {
-    var txt = this;
-    for (var i = 0; i < arguments.length; i++) {
-        var exp = new RegExp('\\{' + (i) + '\\}', 'gm');
-        txt = txt.replace(exp, arguments[i]);
-    }
-    return txt;
+String.prototype.fmt = function() {
+	var txt = this;
+	for (var i = 0; i < arguments.length; i++) {
+		var exp = new RegExp('\\{' + (i) + '\\}', 'gm');
+		txt = txt.replace(exp, arguments[i]);
+	}
+	return txt;
 };
-String.prototype.endsWith = function (suffix) {
+String.prototype.endsWith = function(suffix) {
     return this.indexOf(suffix, this.length - suffix.length) !== -1;
 };
-
 function setInfo(lepra) {
     var lastVote = lepra.karmavotes.pop();
-    if (lastVote.attitude[0] !== "-") {
+    if(lastVote.attitude[0] !== "-"){
         lastVote.attitude = "+" + lastVote.attitude;
     }
-    if (getClickBehavior() === "glagne") {
-        chrome.browserAction.setTitle({
-            title: "Карма: {0} ({1} {2})\nРейтинг: {3}\nМои вещи: {4}/{5}\nИнбокс: {6}/{7}".fmt(
-            lepra.karma, lastVote.login, lastVote.attitude, lepra.rating, lepra.myunreadposts, lepra.myunreadcomms, lepra.inboxunreadposts, lepra.inboxunreadcomms)
-        });
+    if(getClickBehavior() === "glagne"){
+        chrome.browserAction.setTitle({title :
+            "Карма: {0} ({1} {2})\nРейтинг: {3}\nМои вещи: {4}/{5}\nИнбокс: {6}/{7}".fmt(
+                lepra.karma,
+                lastVote.login,
+                lastVote.attitude,
+                lepra.rating,
+                lepra.myunreadposts,
+                lepra.myunreadcomms,
+                lepra.inboxunreadposts,
+                lepra.inboxunreadcomms
+                )});
     }
     var showOnBadge = getShowOnBadge();
-    if (showOnBadge !== "none") {
+    if(showOnBadge !== "none") {
         var badgeText = lepra[showOnBadge] !== '0' ? lepra[showOnBadge] : '';
-        chrome.browserAction.setBadgeText({
-            text: badgeText
-        });
+        chrome.browserAction.setBadgeText({text: badgeText});
     }
 
 }
 
-function updateInfo() {
-    $.ajax({
-        url: "http://leprosorium.ru/api/lepropanel",
-        dataType: "json",
-        error: function (xhr) {
-            if (xhr.readyState == 4 && xhr.status == 404) {
-                setInfo(JSON.parse(xhr.responseText))
-            } else {
-                chrome.browserAction.setTitle({
-                    title: "Балет :("
-                });
-            }
-        }
-    });
+function updateInfo(){
+  $.ajax({
+    url: "http://leprosorium.ru/api/lepropanel",
+  dataType: "json",
+  error: function (xhr) {
+    if(xhr.readyState == 4 && xhr.status == 404){
+      setInfo(JSON.parse(xhr.responseText))
+    } else {
+      chrome.browserAction.setTitle({title : "Балет :("});
+    }
+  }
+  });
 }
--- popup.js
+++ popup.js
@@ -1,74 +1,71 @@
 function setPopupInformation(lepra) {
-    var lastkarmavotes = "Last votes: \r\n";
+	var lastkarmavotes = "Last votes: \r\n";
 
-    for (var i = 1; i <= 3; i++) {
-        var vote = lepra.karmavotes[lepra.karmavotes.length - i];
-        var sign = ""
+	for (var i = 1; i <= 3; i++) {
+		var vote = lepra.karmavotes[lepra.karmavotes.length - i];
+		var sign = ""
 
-        if (vote.attitude[0] !== "-") {
-            sign = "+";
-        }
-        lastkarmavotes += vote.login + " " + sign + vote.attitude + "\r\n";
-    }
-    $("#loading").hide();
-    $("#result").show();
-    $("#piska").append($("<nobr/>", {
-        text: lepra.karma + "/" + lepra.rating
-    })).attr({
-        title: lastkarmavotes
-    });
-    $("#inbox").text(lepra.inboxunreadposts + "/" + lepra.inboxunreadcomms);
-    $("#my").text(lepra.myunreadposts + "/" + lepra.myunreadcomms);
-    $("#glagne").click(function () {
-        gotourl('http://leprosorium.ru/');
-    });
-    $("#things").click(function () {
-        gotourl('http://leprosorium.ru/my/');
-    });
-    $("#inboxes").click(function () {
-        gotourl('http://leprosorium.ru/my/inbox/');
-    });
+		if (vote.attitude[0] !== "-") {
+			sign = "+";
+		}
+		lastkarmavotes += vote.login + " " + sign + vote.attitude + "\r\n";
+	}
+	$("#loading").hide();
+	$("#result").show();
+	$("#piska").append($("<nobr/>", {text : lepra.karma + "/" + lepra.rating})).attr({
+		title: lastkarmavotes
+	});
+	$("#inbox").text(lepra.inboxunreadposts + "/" + lepra.inboxunreadcomms);
+	$("#my").text(lepra.myunreadposts + "/" + lepra.myunreadcomms);
+	$("#glagne").click(function() {
+		gotourl('http://leprosorium.ru/');
+	});
+	$("#things").click(function() {
+		gotourl('http://leprosorium.ru/my/');
+	});
+	$("#inboxes").click(function() {
+		gotourl('http://leprosorium.ru/my/inbox/');
+	});
 }
-
 function gotourl(_url) {
-    chrome.windows.getCurrent(function (w) {
-        chrome.tabs.getAllInWindow(w.id, function (tabs) {
-            for (var i = 0, n = tabs.length; i < n; i += 1) {
-                if (tabs[i].url && tabs[i].url === _url) {
-                    chrome.tabs.update(tabs[i].id, {
-                        selected: true
-                    });
-                    return;
-                }
-            }
-            chrome.tabs.getSelected(w.id, function (tab) {
-                if (tab.url === "chrome://newtab/") {
-                    chrome.tabs.update(tab.id, {
-                        url: _url
-                    });
-                    window.close();
-                } else {
-                    chrome.tabs.create({
-                        url: _url
-                    });
-                }
-            });
-        });
-    });
+	chrome.windows.getCurrent(function(w) {
+		chrome.tabs.getAllInWindow(w.id, function(tabs) {
+			for (var i = 0, n = tabs.length; i < n; i += 1) {
+				if (tabs[i].url && tabs[i].url === _url) {
+					chrome.tabs.update(tabs[i].id, {
+						selected: true
+					});
+					return;
+				}
+			}
+			chrome.tabs.getSelected(w.id, function(tab) {
+				if (tab.url === "chrome://newtab/") {
+					chrome.tabs.update(tab.id, {
+						url: _url
+					});
+					window.close();
+				} else {
+					chrome.tabs.create({
+						url: _url
+					});
+				}
+			});
+		});
+	});
 }
 
-$(function () {
-    if (getClickBehavior() === 'popup') {
-        var lastUpdate = Number(localStorage["lastupdatetime"] || "0");
-        var lastUpdateSuccessed = localStorage["lastupdatesuccess"] || "false";
+$(function() {
+	if (getClickBehavior() === 'popup') {
+		var lastUpdate = Number(localStorage["lastupdatetime"] || "0");
+		var lastUpdateSuccessed = localStorage["lastupdatesuccess"] || "false";
 
-        if (lastUpdateSuccessed === "false" || (new Date().getTime() - lastUpdate) > 300000) {
-            setTimeout(function () {
-                $.ajax({
-                    url: "http://leprosorium.ru/api/lepropanel",
-                    dataType: "json",
-                    error: function (xhr) {
-                        if (xhr.readyState == 4 && xhr.status == 404) {
+		if (lastUpdateSuccessed === "false" || (new Date().getTime() - lastUpdate) > 300000) {
+			setTimeout(function() {
+				$.ajax({
+					url: "http://leprosorium.ru/api/lepropanel",
+					dataType: "json",
+					error: function(xhr) {
+                        if(xhr.readyState == 4 && xhr.status == 404){
                             lepra = JSON.parse(xhr.responseText);
                             setPopupInformation(lepra);
                             localStorage["lastupdatesuccess"] = true;
@@ -80,16 +77,17 @@ $(function () {
                             $("#error").show();
                         }
 
-                    }
-                });
-            }, 0);
-        } else {
-            setPopupInformation(JSON.parse(localStorage["lastupdatelepra"]));
-        }
-    } else {
-        chrome.tabs.create({
-            url: 'http://leprosorium.ru/'
-        });
-        window.close();
-    }
+					}
+				});
+			},
+			0);
+		} else {
+			setPopupInformation(JSON.parse(localStorage["lastupdatelepra"]));
+		}
+	} else {
+		chrome.tabs.create({
+			url: 'http://leprosorium.ru/'
+		});
+		window.close();
+	}
 });
